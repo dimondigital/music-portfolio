d269f3820ac715c2ac72c2924a58c901f3f162c6
Nikolaev <nikolaewdimon@gmail.com>
2023-08-09 19:38:42 +0300

    add new items

--- music-portfolio/src/app/portfolio-element/portfolio-element.ts
+++ music-portfolio/src/app/portfolio-element/portfolio-element.ts
@@ -5,6 +5,7 @@ export interface PortfolioElement {
   genres: string[],
   videoId: string,
   soundcloudMeta: any,
+  soundcloudMeta2: any,
   bandcampMeta: any,
   description: string[],
   links: Link[],

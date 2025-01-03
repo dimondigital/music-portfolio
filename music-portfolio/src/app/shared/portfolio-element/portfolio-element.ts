2912097ccd2e8b5b225efed82bedcd03047b364d
Nikolaev <frontend.niko@gmail.com>
2025-01-03 06:56:15 +0200

    udpate data (services topic)

--- music-portfolio/src/app/shared/portfolio-element/portfolio-element.ts
+++ music-portfolio/src/app/shared/portfolio-element/portfolio-element.ts
@@ -4,6 +4,8 @@ export interface PortfolioElement {
   title: string[],
   genres: string[],
   videoId: string,
+  videoId2: string, // TODO: make array with videoIds later
+  videoId3: string, // TODO: make array with videoIds later
   soundcloudMeta: any,
   soundcloudMeta2: any,
   bandcampMeta: any,
